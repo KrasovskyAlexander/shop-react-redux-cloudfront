7f27deadbadf3af782863d4f017671daa35c081d
Alexander Krasovsky <Alexander_Krasovsky@epam.com>
2022-12-16 03:35:35 +0300

    feat: change api url

--- src/mocks/data.ts
+++ src/mocks/data.ts
@@ -7,38 +7,9 @@ export const products: Product[] = [
   {
     description: "Short Product Description1",
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
+    image: "https://content2.onliner.by/catalog/device/main/ad1fd08115cc6e1b4c289d580d79b406.jpeg",
     price: 24,
     title: "ProductOne",
-  },
-  {
-    description: "Short Product Description7",
-    id: "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
-    price: 15,
-    title: "ProductTitle",
-  },
-  {
-    description: "Short Product Description2",
-    id: "7567ec4b-b10c-48c5-9345-fc73c48a80a3",
-    price: 23,
-    title: "Product",
-  },
-  {
-    description: "Short Product Description4",
-    id: "7567ec4b-b10c-48c5-9345-fc73348a80a1",
-    price: 15,
-    title: "ProductTest",
-  },
-  {
-    description: "Short Product Descriptio1",
-    id: "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
-    price: 23,
-    title: "Product2",
-  },
-  {
-    description: "Short Product Description7",
-    id: "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
-    price: 15,
-    title: "ProductName",
   },
 ];
 
@@ -52,18 +23,10 @@ export const cart: CartItem[] = [
       description: "Short Product Description1",
       id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
       price: 24,
+      image: "https://content2.onliner.by/catalog/device/main/ad1fd08115cc6e1b4c289d580d79b406.jpeg",
       title: "ProductOne",
     },
     count: 2,
-  },
-  {
-    product: {
-      description: "Short Product Description7",
-      id: "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
-      price: 15,
-      title: "ProductName",
-    },
-    count: 5,
   },
 ];
 
